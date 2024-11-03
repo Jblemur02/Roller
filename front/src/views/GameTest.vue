0225b3f0b7f9c5224b6cb97262fcc436d3ca67b4
Jethro Blemur <blemurjethro87@gmail.com>
2024-11-02 20:39:18 -0400

    further customization of play area

--- front/src/views/GameTest.vue
+++ front/src/views/GameTest.vue
@@ -1,15 +1,55 @@
 <template>
   <main>
     <div id="stats">
-      <div id="player">
-        <div id="your-char"></div>
-        <div id="your-minions">
-          <div v-for="minion in yourMinions" :key="minion.id" class="minion">
-            {{ minion.name }}
+      <div id="player" class="character-display">
+        <div id="your-char" class="character">
+          <img :src="yourChar.image" alt="" />
+          {{ yourChar.name }}
+        </div>
+        <div id="your-minions" class="minion-display">
+          <div
+            v-for="minion in yourChar.minions"
+            :key="minion.id"
+            class="minion"
+          >
+            <img :src="minion.image" :alt="minion.name" />
+            <p>{{ minion.name }}</p>
+            <div class="minion-health-bar">
+              <div
+                :style="{
+                  width: `${(minion.currentHealth / minion.maxHealth) * 100}%`,
+                }"
+                class="health-bar"
+              ></div>
+            </div>
           </div>
         </div>
       </div>
-      <div id="opp"></div>
+      <button @click="simulateHealthChanges">Simulate Health Changes</button>
+      <div id="opp" class="character-display">
+        <div id="opp-char" class="character">
+          <img :src="oppChar.image" alt="" />
+          <p>{{ oppChar.name }}</p>
+        </div>
+        <div id="opp-minions" class="minion-display">
+          <div
+            v-for="minion in oppChar.minions"
+            :key="minion.id"
+            class="minion"
+          >
+            <img :src="minion.image" :alt="minion.name" />
+            <p>{{ minion.name }}</p>
+            <div class="minion-health-bar">
+              <div
+                :style="{
+                  width: `${(minion.currentHealth / minion.maxHealth) * 100}%`,
+                }"
+                class="health-bar"
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <div id="board">
       <div v-for="(col, colIndex) in 5" :key="colIndex" class="column">
@@ -42,15 +82,101 @@ export default {
         [{}, {}, {}, {}, {}],
         [{}, {}, {}, {}, {}],
       ],
-      yourMinions: [],
-      opponentMinions: [],
+      yourChar: {
+        id: 1,
+        name: 'Your Character',
+        image:
+          'https://ik.imagekit.io/roller/packs/images/iron_helmet.jpg?updatedAt=1730234852000',
+        maxHealth: 200,
+        currentHealth: 100,
+        minions: [
+          {
+            id: 1,
+            name: 'Minion A',
+            image: 'https://picsum.photos/seed/picsum/200/300',
+            maxHealth: 100,
+            currentHealth: 90,
+          },
+          {
+            id: 2,
+            name: 'Minion B',
+            image: 'https://picsum.photos/seed/picsum/200/300',
+            maxHealth: 100,
+            currentHealth: 90,
+          },
+          {
+            id: 3,
+            name: 'Minion C',
+            image: 'https://picsum.photos/seed/picsum/200/300',
+            maxHealth: 100,
+            currentHealth: 90,
+          },
+          {
+            id: 4,
+            name: 'Minion D',
+            image: 'https://picsum.photos/seed/picsum/200/300',
+            maxHealth: 100,
+            currentHealth: 90,
+          },
+          {
+            id: 5,
+            name: 'Minion E',
+            image: 'https://picsum.photos/seed/picsum/200/300',
+            maxHealth: 100,
+            currentHealth: 90,
+          },
+        ],
+      },
+      oppChar: {
+        id: 2,
+        name: 'Opponent Character',
+        image:
+          'https://ik.imagekit.io/roller/packs/images/iron_helmet.jpg?updatedAt=1730234852000',
+        maxHealth: 200,
+        currentHealth: 150,
+        minions: [
+          {
+            id: 1,
+            name: 'Enemy 1',
+            image: 'https://picsum.photos/seed/picsum/200/300',
+            maxHealth: 100,
+            currentHealth: 90,
+          },
+          {
+            id: 2,
+            name: 'Enemy 2',
+            image: 'https://picsum.photos/seed/picsum/200/300',
+            maxHealth: 100,
+            currentHealth: 90,
+          },
+          {
+            id: 3,
+            name: 'Enemy 3',
+            image: 'https://picsum.photos/seed/picsum/200/300',
+            maxHealth: 100,
+            currentHealth: 90,
+          },
+        ],
+      },
     }
   },
   methods: {
     handleSquareClick(col, row) {
       console.log(`Clicked square at row ${row}, column ${col + 1}`)
     },
+    simulateHealthChange(minion) {
+      const healthChange = Math.floor(Math.random() * 50) - 25
+      minion.currentHealth = Math.max(
+        0,
+        Math.min(minion.maxHealth, minion.currentHealth + healthChange),
+      )
+    },
+    simulateHealthChanges() {
+      this.yourChar.minions.forEach(minion => this.simulateHealthChange(minion))
+      this.oppChar.minions.forEach(minion => this.simulateHealthChange(minion))
+    },
   },
+  computed: {},
 }
 </script>
 
@@ -65,12 +191,95 @@ main {
 
 #stats {
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+  flex-direction: column-reverse;
+  height: 100%;
   margin-bottom: 20px;
   height: 100%;
-  border: 1px solid red;
   grid-area: stats;
+  padding: 20px;
+}
+
+.character-display {
+  width: 90%;
+  margin: 0 auto;
+  box-shadow: var(--shadow);
+  border-radius: 19px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  height: 30vh;
+  background-color: var(--inverse);
+  gap: 10px;
+}
+
+.character {
+  box-shadow: var(--shadow);
+  width: 100%;
+  height: 50%;
+  border-radius: 19px;
+  display: flex;
+  padding: 10px;
+}
+
+.character img {
+  width: 20%;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+  box-shadow: var(--shadow);
+}
+
+.minion-display {
+  display: flex;
+  gap: 10px;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 10px;
+  height: 50%;
+  width: 100%;
+  border-radius: 10px;
+}
+
+.minion {
+  box-shadow: var(--shadow);
+  height: 100%;
+  width: 100%;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 5px;
+}
+
+.minion img {
+  height: 50%;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+}
+
+.minion-health-bar {
+  height: 10%;
+  width: 100%;
+  box-shadow: var(--shadow);
+  border-radius: 5px;
+  background-color: rgb(207, 29, 29);
+}
+
+.health-bar {
+  height: 100%;
+  background-color: red;
+  transition: width 0.3s ease;
+  box-shadow: 0px 0px 59px 11px rgba(255, 0, 0, 1) inset;
+  -webkit-box-shadow: 0px 0px 59px 11px rgba(255, 0, 0, 1) inset;
+  -moz-box-shadow: 0px 0px 59px 11px rgba(255, 0, 0, 1) inset;
+}
+
+#player {
+  flex-direction: column-reverse;
 }
 
 #board {
@@ -89,15 +298,21 @@ main {
 .square {
   width: 100%;
   aspect-ratio: 1/1;
-  border: 1px solid var(--primary);
+  border: 1px solid black;
   background-color: var(--inverse);
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 10px;
-  box-shadow: 0px 0px 5px 15px rgba(24, 46, 242, 0.75) inset;
-  -webkit-box-shadow: 0px 0px 5px 15px rgba(24, 46, 242, 0.75) inset;
-  -moz-box-shadow: 0px 0px 5px 15px rgba(24, 46, 242, 0.75) inset;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  box-shadow: 0px 0px 35px 5px rgba(71, 106, 117, 1) inset;
+  -webkit-box-shadow: 0px 0px 35px 5px rgba(71, 106, 117, 1) inset;
+  -moz-box-shadow: 0px 0px 35px 5px rgba(71, 106, 117, 1) inset;
+}
+
+.square:hover {
+  border: 1px solid var(--primaryh);
 }
 
 #moves {
