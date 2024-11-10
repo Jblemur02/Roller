09a3c8efc3d26cdff709f3701a3a130d62460f37
Jethro Blemur <blemurjethro87@gmail.com>
2024-11-09 21:55:50 -0500

    backend fixes

--- back/sqldb.js
+++ back/sqldb.js
@@ -1,16 +1,15 @@
-// // mysqlConnection.js
-// const mysql = require("mysql2");
+// mysqlConnection.js
+const mysql = require("mysql2");
 
-// // Write pool for write operations
-// const pool = mysql.createPool({
-//   host: "mysql-roller-treasureroller.e.aivencloud.com",
-//   user: "avnadmin",
-//   password: "AVNS_hQPu2sxujQMBxGNe6B6",
-//   database: "roller",
-//   port: 24356,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
+// Write pool for write operations
+const pool = mysql.createPool({
+  host: "mysql-roller-treasureroller.e.aivencloud.com",
+  user: "avnadmin",
+  database: "roller",
+  port: 24356,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-// module.exports = pool;
+module.exports = pool;
